cd20978121fd3acd441d00bf42755dabb51a63a1
Sabrina0531 <rm99570@fiap.com.br>
2023-05-17 09:56:11 -0300

    mensagem atualizada

--- js/login.js
+++ js/login.js
@@ -1,25 +1,40 @@
-//Gerando um token com Math.
-let tokenGerado = Math.random().toString(16).substring(2);
-//DECLARANDO OBJETOS
-const usuario1 = {
-    nomeUsuario : "admin",
-    senhaUsuario: "12345",
-    gravaDados : true,
-    token : tokenGerado
-}
 
-tokenGerado = Math.random().toString(16).substring(2);
-//DECLARANDO OBJETOS
-const usuario2 = {
-    nomeUsuario : "user",
-    senhaUsuario: "12345",
-    gravaDados : true,
-    token : tokenGerado
-}
+// //CRIANDO LISTA DE OBJETOS
+// let listaDeUsuarios = [
+//     {
+//         nomeCompleto : "Denden da Silva",
+//         nomeUsuario : "denden",
+//         senhaUsuario : "123456"
+//     },
+//     {
+//         nomeCompleto : "Gersu da Silva",
+//         nomeUsuario : "gege",
+//         senhaUsuario : "123456"
+//     },
+//     {
+//         nomeCompleto : "José da Silva",
+//         nomeUsuario : "jose",
+//         senhaUsuario : "123456"
+//     },
+//     {
+//         nomeCompleto : "Paulo das Couves",
+//         nomeUsuario : "paulo",
+//         senhaUsuario : "123456"
+//     },
+//     {
+//         nomeCompleto : "Mary Help",
+//         nomeUsuario : "mary",
+//         senhaUsuario : "123456"
+//     },
+//     {
+//         nomeCompleto : "Pedro Silva",
+//         nomeUsuario : "pedro",
+//         senhaUsuario : "123456"
+//     }
+// ];
 
-let listaDeUsuarios = [];
-listaDeUsuarios.push(usuario1);
-listaDeUsuarios.push(usuario2);
+// //GUARDAR A LISTA DE OBJETOS NO LOCAL-STORAGE
+// localStorage.setItem( "listaUser",  JSON.stringify(listaDeUsuarios));
 
 addEventListener("click", (evt)=>{
     const inputUser = document.querySelector("#idUser");
@@ -28,7 +43,10 @@ addEventListener("click", (evt)=>{
     if(evt.target.id == "btnSubmit"){
         
         try {
-            
+          
+            //Recuperar a lista de usuários do LocalStorage
+            let listaDeUsuarios = JSON.parse(localStorage.getItem("listaUser"));
+
             listaDeUsuarios.forEach((usuario)=>{
                 
                 if(inputUser.value == usuario.nomeUsuario && inputPass.value == usuario.senhaUsuario){
@@ -43,21 +61,31 @@ addEventListener("click", (evt)=>{
             if(msg == "VALIDADO"){
                 msgError.setAttribute("style","color:#00ff00;");
                 msgError.innerHTML = "<span><strong>Login efetuado com Sucesso!</strong></span>";
+                
+                //Redirect : Espera 3 segundos antes de redirecionar
+                setTimeout( function(){
+                    window.location.href = "../pages/sucesso.html";
+                }, 3000);
+
             }else{
                 msgError.setAttribute("style","color:#ff0000;");
                 msgError.innerHTML = "<span><strong>Usuário ou senha inválidos!</strong></span>";
             }
         }
 
-    }else if(evt.target.className == "fa fa-eye" || evt.target.className == "fa fa-eye-slash"){
-        
-        if(inputPass.getAttribute("type") == "password"){
-            evt.target.setAttribute("class", "fa fa-eye-slash");
-            inputPass.setAttribute("type", "text");
+//DESAFIO:
+//Troque o ícone do olho aberto assim que o usuário clicar para ver a senha pelo ícone de olho cortado.
 
+    }else if(evt.target.className == "fa fa-eye" || evt.target.className == "fa fa-eye-slash"){
+        //Mostrando a senha!
+        if(inputPass.getAttribute("type") == "password"){
+            evt.target.setAttribute("class","fa fa-eye-slash");
+            inputPass.setAttribute("type","text");
         }else{
-            inputPass.setAttribute("type", "password");
-            evt.target.setAttribute("class", "fa fa-eye");
+        //Ocultando a senha!
+        evt.target.setAttribute("class","fa fa-eye");
+            inputPass.setAttribute("type","password");
         }
+        
     }
 });
